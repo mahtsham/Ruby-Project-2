a81a62bb18ad3d5bc94f317c0db10dd1dce62046
mahtsham <muhammadahtsham357@gmail.com>
2021-02-22 20:25:46 +0500

    remove rubocop errors

--- enum.rb
+++ enum.rb
@@ -41,33 +41,3 @@ module Enumerable
     true
   end
 end
-
-
-puts
-puts '=========Test Cases========='
-puts
-puts 'my_each'
-print [1, 2, 3].my_each { |elem| print "#{elem + 1} " } # => 2 3 4
-puts
-
-puts
-puts 'my_each_with_index'
-print [1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" } # => 1 : 0, 2 : 1, 3 : 2
-puts
-
-puts 'my_select'
-p [1, 2, 3, 8].my_select(&:even?) # => [2, 8]
-p [0, 2018, 1994, -7].my_select(&:positive?) # => [2018, 1994]
-p [6, 11, 13].my_select(&:odd?) # => [11, 13]
-puts
-
-puts 'my_all?'
-p [3, 5, 7, 11].my_all?(&:odd?) # => true
-p [-8, -9, -6].my_all?(&:negative?) # => true
-p [3, 5, 8, 11].my_all?(&:odd?) # => false
-p [-8, -9, -6, 0].my_all?(&:negative?) # => false
-# test cases required by tse reviewer
-p [1, 2, 3, 4, 5].my_all? # => true
-p [1, 2, 3].my_all?(Integer) # => true
-p %w[dog door rod blade].my_all?(/d/) # => true
-p [1, 1, 1].my_all?(1) # => true
