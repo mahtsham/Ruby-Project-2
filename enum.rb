e7b5be0dbbc8c45f95f4bbebecc35172acf9ea87
mahtsham <muhammadahtsham357@gmail.com>
2021-02-26 03:42:20 +0500

    add changes to my_map

--- enum.rb
+++ enum.rb
@@ -113,11 +113,3 @@ end
 def multiply_els(array)
   array.my_inject(1) { |product, i| product * i }
 end
-#8. my_map
-puts "my_map"
-p [1,2,3].my_map { |n| 2 * n } # => [2,4,6]
-p ["Hey", "Jude"].my_map { |word| word + "?" } # => ["Hey?", "Jude?"]
-p [false, true].my_map { |bool| !bool } # => [true, false]
-my_proc = Proc.new {|num| num > 10 }
-p [18, 22, 5, 6] .my_map(my_proc) {|num| num < 10 } # => true true false false
-puts
